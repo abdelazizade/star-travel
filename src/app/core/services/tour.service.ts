099fa36c594e86794af3fa7b4aa67ba3b2e4889b
abdelazizade <abdelaziz.adel.m@gmail.com>
2026-02-27 14:14:18 +0200

    [add] integration with api

--- src/app/core/services/tour.service.ts
+++ src/app/core/services/tour.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TourService {
-  constructor() { }
+  private readonly http = inject(HttpClient);
+  private apiUrl = 'https://repository.star-travel.io/api/v1/cards';
+
   
-  // Service methods will be added here
+  getAllTours(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
 
 
